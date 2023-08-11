b1fbd928c73aea007cc1920770a61a6b184640c7
mays alwattar <maysmm199@gmail.com>
2023-08-11 15:12:48 -0700

    upload the repo class

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.css";
+import Header from "./components/Header";
+import Paragraph from "./components/Paragraph";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Paragraph />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Paragraph.jsx
@@ -0,0 +1,11 @@
+import React from 'react'; 
+ 
+const Paragraph = () => { 
+  return ( 
+    <div> 
+      <p>This is the paragraph in the middle of the app</p> 
+    </div> 
+  ); 
+}; 
+ 
+export default Paragraph;
